a0fa48b58f9ff92d4592c5310348ea8c351f4afb
Ich <Ich@DESKTOP-FEMGL0I>
2019-01-16 20:14:11 +0100

    ThreadId hinzugefügt

--- SocketServer2/src/server/Utils.java
+++ SocketServer2/src/server/Utils.java
@@ -14,14 +14,15 @@ public class Utils {
 		
 		ConsoleHandler handler = new ConsoleHandler();
 		handler.setFormatter(new SimpleFormatter() {
-		    private static final String format = "[%1$tF %1$tT] [%2$-7s] %3$s %n";
+		    private static final String format = "[%1$tF %1$tT] [%2$s] <%4$s> %3$s %n";
 		
 		    @Override
 		    public synchronized String format(LogRecord lr) {
 		        return String.format(format,
 		                new Date(lr.getMillis()),
 		                lr.getLevel().getLocalizedName(),
-		                lr.getMessage()
+		                "Thread-" + lr.getThreadID() + ": " + lr.getMessage(),
+		                lr.getLoggerName()
 		        );
 		    }
 		});
